de26892bb86b7111a17b88a4965228518f2f67a5
jmossr <jmossroot@gmail.com>
2023-12-08 00:20:07 +0100

    add valueAbove and valueBelow building functions

---
+++ test/DCSCallsValueBelowBuilderTest.lua
@@ -0,0 +1,38 @@
+local config = {
+    extensions = { 'builder', 'DCS-calls' },
+    debug = false,
+}
+
+local selfData = { Name = 'TestUnit' }
+
+return { test = function()
+    initFlightAssistantTestConfig(config)
+
+    local withLogEvents = true
+
+    setupFlightAssistant(config, selfData, withLogEvents)
+    checkEvents('A')
+
+    expect('Export.LoGetSelfData').andReturn(selfData)
+    expect('onSimulationFrame').andReturn('onDeviceArgument(25, 3001).valueBelow(0.1).call(function(val) checkEvent("!!!"..tostring(val).." <= 0.1"); end)')
+    fireUserCallback('onSimulationFrame')
+    checkEvents('B')
+
+    expect('Export.LoGetSelfData').andReturn(selfData)
+    expect('Export.GetDevice(25)').andReturn({ get_argument_value = function(_, a) return checkEvent('getDeviceArgumentValue(' .. a .. ')'); end, })
+    expect('getDeviceArgumentValue(3001)').andReturn(0.05)
+    expect('!!!0.05 <= 0.1')
+    expect('onSimulationFrame')
+    fireUserCallback('onSimulationFrame')
+    checkEvents('C')
+
+    expect('Export.LoGetSelfData').andReturn(selfData)
+    expect('Export.GetDevice(25)').andReturn({ get_argument_value = function(_, a) return checkEvent('getDeviceArgumentValue(' .. a .. ')'); end, })
+    expect('getDeviceArgumentValue(3001)').andReturn(0.101)
+    expect('onSimulationFrame')
+    fireUserCallback('onSimulationFrame')
+    checkEvents('D')
+
+    assert(withLogEvents, 'withLogEvents must be true for this test')
+
+end }
